344ae008d42da3e30819d0bafdbb6a1835ef9fc8
PradeepJ0710 <112463741+PradeepJ0710@users.noreply.github.com>
2022-08-31 14:04:04 +0530

    Update NonEquijoinSample1.sql

--- NonEquijoinSample1.sql
+++ NonEquijoinSample1.sql
@@ -6,3 +6,5 @@ from employees e join jobs j
 on e.salary >= j.max_salary
 where j.job_id = 'SA_REP'
 order by e.salary desc, e.first_name;
+
+#This is an example of a Non-Equijoin because, the ON clause contains a non-equality condition.
