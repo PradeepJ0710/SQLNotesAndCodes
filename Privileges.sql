30d9b01ff94453ad68a5f57325c081fac99983d9
PradeepJ0710 <112463741+PradeepJ0710@users.noreply.github.com>
2022-10-20 11:19:13 +0530

    Add files via upload

---
+++ Privileges.sql
@@ -0,0 +1,21 @@
+select * from session_privs;
+
+select * from dictionary where table_name like '%employee%';
+
+select * from user_sys_privs;
+
+select * from user_col_privs;
+
+grant all on session_privs to hr;
+
+show con_name;
+
+select * from role_sys_privs where role = 'DBA';
+
+select * from dict where upper(table_name) like '%TAB%';
+
+select * from user_role_privs;
+
+grant select any table to hr;
+
+select * from all_users order by username;
